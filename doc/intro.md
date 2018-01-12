f28afb1e253535847be5d54f9d698baf848ac28d
komcrad <konradt95@gmail.com>
2018-01-12 13:03:44 -0600

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to komcrad-utils
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/komcrad_utils/core_test.clj
@@ -0,0 +1,7 @@
+(ns komcrad-utils.core-test
+  (:require [clojure.test :refer :all]
+            [komcrad-utils.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
