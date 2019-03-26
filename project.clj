05ac682e66e6c4ab49ce5c3255a0220c08fe86e6
komcrad <konradt95@gmail.com>
2019-03-26 11:35:27 -0500

    Adds functions for dealing with streams that will be updated as time goes on and you want to check it as it changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject komcrad-utils "0.8.0"
+(defproject komcrad-utils "0.9.0"
   :description "komcrad's utilities"
   :url "https://github.com/komcrad/komcrad-utils"
   :license {:name "GNU Lesser General Public License"
