7a27b229e4f026cdc961e1582a2878ae77ec46df
SgtZdog <kevin@zemon.name>
2019-11-07 12:56:12 -0600

    [MAN-1720]

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "https://github.com/komcrad/komcrad-utils"
   :license {:name "GNU Lesser General Public License"
             :url "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :deploy-repositories [["releases" {:url "https://mvn.redlion.net/repository/maven-releases"
+                                     :sign-releases false}]
+                        ["snapshots" {:url "https://mvn.redlion.net/repository/maven-snapshots"
+                                      :sign-releases false}]])
