9580f7f48e823ee88362603d6f8aec2b307453f4
SgtZdog <kevin@zemon.name>
2019-11-07 13:03:24 -0600

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject komcrad-utils "0.9.0"
   :description "komcrad's utilities"
-  :url "https://github.com/komcrad/komcrad-utils"
+  :url "https://github.com/wsbu/komcrad-utils"
   :license {:name "GNU Lesser General Public License"
             :url "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
